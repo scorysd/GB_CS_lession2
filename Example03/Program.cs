8cd95c0f3c528a47221baa67cf8e40f771a87667
Roman <toolatet@gmail.com>
2022-04-16 17:22:37 +0700

    do exercise 3

---
+++ Example03/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+if (number <= 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (number < 999)
+{
+    Console.WriteLine(number%10);
+}
+else
+{
+    do 
+    {
+    number = number/10;
+    }
+    while (number > 999);
+    number = number%10;
+    Console.WriteLine(number);
+} 
