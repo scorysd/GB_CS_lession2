28e4f21bf4d193060a3682e1e758606e2d594368
Roman <toolatet@gmail.com>
2022-04-16 17:22:59 +0700

    do exercise 4

---
+++ Example04/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число, соотвтетствуещее дню недели: ");
+int date = Convert.ToInt32(Console.ReadLine());
+if (date>7 | date<0)
+{
+    Console.WriteLine($"Введеное число: {date} не соответствует дню недели"); 
+}
+else if (date==6 | date==7)
+{
+    Console.WriteLine($"Выходной"); 
+}
+else 
+{
+    Console.WriteLine($"Нет, не выходной"); 
+}    
+    
